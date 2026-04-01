d0290e35ed791318fdd067876e10c184f78ef7f6
Chase Counsell <cjcounsell@gmail.com>
2026-04-01 11:21:26 -0400

    automated dev commit

--- dotfiles/common/.config/zsh/env.zsh
+++ dotfiles/common/.config/zsh/env.zsh
@@ -13,7 +13,8 @@ export STARSHIP_CONFIG="$HOME/.config/starship.toml"
 export PHP_INI_SCAN_DIR="$HOME/.config/herd-lite/bin:$PHP_INI_SCAN_DIR"
 
 # Project directories for tmux-sessionizer
-export CODE_DIRS="$HOME:$HOME/personal:$HOME/work:$HOME/vaults"
+# ~/work/mexp included so sessionizer finds worktrees as top-level dirs
+export CODE_DIRS="$HOME:$HOME/personal:$HOME/work:$HOME/work/mexp:$HOME/vaults"
 
 # Deno environment
 [[ -f "$HOME/.deno/env" ]] && source "$HOME/.deno/env"
