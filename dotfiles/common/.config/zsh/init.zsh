4cfc92e241e65f5b2336c24ad310302273fa24b9
Chase Counsell <cjcounsell@gmail.com>
2026-04-02 09:38:16 -0400

    automated dev commit

--- dotfiles/common/.config/zsh/init.zsh
+++ dotfiles/common/.config/zsh/init.zsh
@@ -10,33 +10,3 @@ command -v mise >/dev/null 2>&1 && eval "$(mise activate zsh)"
 command -v tmuxifier >/dev/null 2>&1 && eval "$(tmuxifier init -)"
 command -v starship >/dev/null 2>&1 && eval "$(starship init zsh)"
 command -v zoxide >/dev/null 2>&1 && eval "$(zoxide init --cmd cd zsh)"
-
-. "$HOME/.local/share/../bin/env"
-
-#compdef opencode
-###-begin-opencode-completions-###
-#
-# yargs command completion script
-#
-# Installation: opencode completion >> ~/.zshrc
-#    or opencode completion >> ~/.zprofile on OSX.
-#
-_opencode_yargs_completions()
-{
-  local reply
-  local si=$IFS
-  IFS=$'
-' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" opencode --get-yargs-completions "${words[@]}"))
-  IFS=$si
-  if [[ ${#reply} -gt 0 ]]; then
-    _describe 'values' reply
-  else
-    _default
-  fi
-}
-if [[ "'${zsh_eval_context[-1]}" == "loadautofunc" ]]; then
-  _opencode_yargs_completions "$@"
-else
-  compdef _opencode_yargs_completions opencode
-fi
-###-end-opencode-completions-###
